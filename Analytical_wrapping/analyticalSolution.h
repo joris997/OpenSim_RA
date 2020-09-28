ce46e313a9408d91cd1308a5fda94500e6f2051d
Adam Kewley <contact@adamkewley.com>
2020-09-28 11:05:35 +0100

    Fixed missing include in analyticalSolution.h

--- Analytical_wrapping/analyticalSolution.h
+++ Analytical_wrapping/analyticalSolution.h
@@ -1,6 +1,8 @@
 #ifndef ANALYTICAL_WRAPPING_ANALYTICALSOLUTION_H
 #define ANALYTICAL_WRAPPING_ANALYTICALSOLUTION_H
 
+#include "testCase.h"
+
 double analyticalSolution(double leftHeight, const testCase& aCase, bool useNumerical);
 
 #endif //ANALYTICAL_WRAPPING_ANALYTICALSOLUTION_H
